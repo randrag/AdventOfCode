215e56641b139bb413c58fea52fedd02d58a3989
Roland Andrag <roland.andrag@gmail.com>
2020-12-06 08:32:26 +0200

    Clean up day 6

--- Y2020/Day6.fs
+++ Y2020/Day6.fs
@@ -1,38 +1,26 @@
 namespace AdventOfCode
 
-
-module List2 =
-    let innerJoin (l1 : List<'a>) (l2 : List<'b>) (compareFn : 'a -> 'b -> bool) =
-      l1
-      |> List.collect (fun e1 ->
-           l2
-           |> List.filter (compareFn e1)
-           |> List.map (fun e2 -> e1, e2)
-        )
-
-
 module Day6 =
-  let loadLines =
-    System.IO.File.ReadLines("/Users/roland/Code/AdventOfCode/Y2020/Day6Input.txt")
-    |> List.ofSeq
-    |> List.map List.ofSeq
-
 
   let run () =
-    loadLines
+
+    let input = System.IO.File.ReadLines("/Users/roland/Code/AdventOfCode/Y2020/Day6Input.txt") |> List.ofSeq
+
+    let stringToCharList = List.ofSeq
+
+    input
+    |> List.map stringToCharList
     |> List.splitMultipleOn ((=) [])
-    |> List.map (List.fold (List.append) [] >> Seq.distinct >> Seq.length)
+    |> List.map (List.reduce List.append >> List.distinct >> List.length)
     |> List.sum
     |> ps "Part 1 answer: "
 
-    let intersect l1 l2 =
-      List2.innerJoin l1 l2 (=)
-      |> List.map fst
+    let intersect l1 l2 = l1 |> List.collect (fun e1 -> l2 |> List.filter ((=) e1))
 
-    loadLines
+    input
+    |> List.map stringToCharList
     |> List.splitMultipleOn ((=) [])
-    |> List.map (fun groupList -> List.fold intersect (List.head groupList) groupList) // don't like the List.head here
-    |> List.map (List.length)
+    |> List.map (List.reduce intersect)
+    |> List.map List.length
     |> List.sum
-    |> ps "Part 2: "
-
+    |> ps "Part 2 answer: "
