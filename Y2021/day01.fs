7b7ebdd3ce97e9c6c73d68dd983cdf34748bece9
Roland Andrag <roland.andrag@gmail.com>
2021-12-01 07:21:09 +0200

    Day 1

--- Y2021/day01.fs
+++ Y2021/day01.fs
@@ -2,14 +2,27 @@ namespace AdventOfCode.Y2021
 
 open Helpers
 
-
 module Day01 =
-  let loadLines = System.IO.File.ReadLines("/Users/roland/Code/AdventOfCode/Y2015/input01.txt") |> Seq.concat
+  let loadLines () = System.IO.File.ReadLines("/Users/roland/Code/AdventOfCode/Y2021/input01.txt") |> Seq.map int64
 
   module Part1 =
-    ()
+    let go () =
+      loadLines ()
+      |> Seq.pairwise
+      |> Seq.map (fun (a,b) -> if a < b then 1 else 0)
+      |> Seq.sum
+      |> ps "Answer 1: "
+
   module Part2 =
-    ()
+    let go () =
+      loadLines ()
+      |> Seq.windowed 3
+      |> Seq.map Seq.sum
+      |> Seq.pairwise
+      |> Seq.map (fun (a,b) -> if a < b then 1 else 0)
+      |> Seq.sum
+      |> ps "Answer 2: "
 
   let run () =
-    () //    printfn $"Floor: {Part1.go ()}"
+    Part1.go ()
+    Part2.go ()
