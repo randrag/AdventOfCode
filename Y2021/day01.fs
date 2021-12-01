7d5a4086871fbfe293ecff14a9f878e11e0753d8
Roland Andrag <roland.andrag@gmail.com>
2021-12-01 07:22:51 +0200

    Cleanup

--- Y2021/day01.fs
+++ Y2021/day01.fs
@@ -3,24 +3,22 @@ namespace AdventOfCode.Y2021
 open Helpers
 
 module Day01 =
-  let loadLines () = System.IO.File.ReadLines("/Users/roland/Code/AdventOfCode/Y2021/input01.txt") |> Seq.map int64
+  let getInput () = System.IO.File.ReadLines("/Users/roland/Code/AdventOfCode/Y2021/input01.txt") |> Seq.map int64
 
   module Part1 =
     let go () =
-      loadLines ()
+      getInput ()
       |> Seq.pairwise
-      |> Seq.map (fun (a,b) -> if a < b then 1 else 0)
-      |> Seq.sum
+      |> Seq.sumBy (fun (a,b) -> if a < b then 1 else 0)
       |> ps "Answer 1: "
 
   module Part2 =
     let go () =
-      loadLines ()
+      getInput ()
       |> Seq.windowed 3
       |> Seq.map Seq.sum
       |> Seq.pairwise
-      |> Seq.map (fun (a,b) -> if a < b then 1 else 0)
-      |> Seq.sum
+      |> Seq.sumBy (fun (a,b) -> if a < b then 1 else 0)
       |> ps "Answer 2: "
 
   let run () =
