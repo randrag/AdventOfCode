7cea59635c9fa63410c5f1cb8b18283945934875
Roland Andrag <roland.andrag@gmail.com>
2021-12-02 17:26:24 +0200

    Clean up day 2

--- Y2021/day02.fs
+++ Y2021/day02.fs
@@ -4,22 +4,22 @@ open Helpers
 
 module Day02 =
   let getInput () =
-     System.IO.File.ReadLines("/Users/roland/Code/AdventOfCode/Y2021/input02.txt")
-    |> Seq.map (fun s -> s.Split(' '))
-    |> Seq.map (Seq.toList)
-    |> Seq.map (fun l ->
-      match l with
-      | [dir; dis] -> dir, dis |> int64
-      | _ -> failwith "bang"
-      )
+    System.IO.File.ReadLines("/Users/roland/Code/AdventOfCode/Y2021/input02.txt")
+    |> Seq.map (fun s ->
+        s.Split(' ')
+        |> Seq.toList
+        |> function
+           | [dir; dis] -> dir, dis |> int64
+           | _ -> failwith "bang"
+        )
 
   module Part1 =
     let move (pos, depth) (direction, distance) =
       match direction with
       | "forward" -> (pos + distance, depth)
-      | "down" -> (pos, depth + distance)
-      | "up" -> (pos, depth - distance)
-      | _ -> failwith "bang2"
+      | "down"    -> (pos, depth + distance)
+      | "up"      -> (pos, depth - distance)
+      | _         -> failwith "bang2"
 
     let go () =
       getInput ()
@@ -29,10 +29,10 @@ module Day02 =
   module Part2 =
     let move (pos, depth, aim) (direction, x) =
       match direction with
-      | "down" -> (pos, depth, aim + x)
-      | "up" -> (pos, depth, aim - x)
+      | "down"    -> (pos, depth, aim + x)
+      | "up"      -> (pos, depth, aim - x)
       | "forward" -> (pos + x, depth + aim * x, aim)
-      | _ -> failwith "bang2"
+      | _         -> failwith "bang2"
 
     let go () =
       getInput ()
