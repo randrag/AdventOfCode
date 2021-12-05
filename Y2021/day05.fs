c20e54e8833fcba7e8b392aeb5afaac6f871ca36
Roland Andrag <roland.andrag@gmail.com>
2021-12-05 07:45:48 +0200

    Clean up day 5

--- Y2021/day05.fs
+++ Y2021/day05.fs
@@ -14,10 +14,10 @@ module Day05 =
             |> Array.toList
             |> List.map int32
             |> function
-                | [a;b] -> a,b
+                | [x;y] -> x,y
                 | _ -> failwith "bang1" )
         |> function
-                | [a;b] -> a,b
+                | [startPoint;endPoint] -> startPoint,endPoint
                 | _ -> failwith "bang2" )
 
   let countCoveredPoint pointMap (x,y) =
@@ -25,7 +25,7 @@ module Day05 =
     pointMap |> Map.add (x,y) (currentCount + 1)
 
   module Part1 =
-    let findCoveredPoints ((x1, y1), (x2, y2)) =
+    let findPointsCoveredByLine ((x1, y1), (x2, y2)) =
       if x1 = x2 then
         let ys = if y1 <= y2 then [y1 .. y2] else [y2 .. y1]
         ys |> List.map (fun y -> (x1, y))
@@ -37,13 +37,12 @@ module Day05 =
     let go () =
       getInput ()
       |> List.filter (fun ((x1, y1), (x2, y2)) -> x1 = x2 || y1 = y2)
-      |> List.collect findCoveredPoints
+      |> List.collect findPointsCoveredByLine
       |> List.fold countCoveredPoint Map.empty
       |> Map.fold' (fun total count -> if count >= 2 then total + 1 else total) 0
 
-
   module Part2 =
-    let findCoveredPoints ((x1, y1), (x2, y2)) =
+    let findPointsCoveredByLine ((x1, y1), (x2, y2)) =
       if x1 = x2 then
         let ys = if y1 <= y2 then [y1 .. y2] else [y2 .. y1]
         ys |> List.map (fun y -> (x1, y))
@@ -51,13 +50,13 @@ module Day05 =
         let xs = if x1 <= x2 then [x1 .. x2] else [x2 .. x1]
         xs |> List.map (fun x -> (x, y1))
       else
-        let xs = if x1 <= x2 then [x1 .. x2] else [x2 .. x1] |> List.rev
-        let ys = if y1 <= y2 then [y1 .. y2] else [y2 .. y1] |> List.rev
+        let xs = if x1 <= x2 then [x1 .. x2] else [x1 .. -1 .. x2]
+        let ys = if y1 <= y2 then [y1 .. y2] else [y1 .. -1 .. y2]
         List.zip xs ys
 
     let go () =
       getInput ()
-      |> List.collect findCoveredPoints
+      |> List.collect findPointsCoveredByLine
       |> List.fold countCoveredPoint Map.empty
       |> Map.fold' (fun total count -> if count >= 2 then total + 1 else total) 0
 
