eaba06a88bb06f7320229c65a77c5fc49b89e631
Roland Andrag <roland.andrag@gmail.com>
2021-12-06 17:19:15 +0200

    Minor cleanup

--- Y2021/day06.fs
+++ Y2021/day06.fs
@@ -5,11 +5,11 @@ open Helpers
 module Day06 =
   let getInput () =
     System.IO.File.ReadLines("/Users/roland/Code/AdventOfCode/Y2021/input06.txt")
-    |> Seq.head |> fun s -> s.Split(',') |> Array.toList |> List.map int64
+    |> Seq.head |> fun s -> s.Split(',') |> Array.toList |> List.map int64 |> List.map (fun age -> (age, 1L))
 
   let ageAndSpawn (age, count) = if age > 0L then [(age-1L, count)] else [(6L, count); (8L, count)]
 
-  let rec ageNDays n ( l : List<int64 * int64> ) =
+  let rec ageNDays n ( l : List<int64 * int64> ) = // takes a list of (age, count)
     if n = 0 then l
     else
       l
@@ -19,5 +19,5 @@ module Day06 =
       |> ageNDays (n-1)
 
   let run () =
-    getInput () |> List.map (fun age -> (age, 1L)) |> ageNDays  80 |> List.sumBy snd |> ps "Part 1: "
-    getInput () |> List.map (fun age -> (age, 1L)) |> ageNDays 256 |> List.sumBy snd |> ps "Part 2: "
+    getInput () |> ageNDays  80 |> List.sumBy snd |> ps "Part 1: "
+    getInput () |> ageNDays 256 |> List.sumBy snd |> ps "Part 2: "
