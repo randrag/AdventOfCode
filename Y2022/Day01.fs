3e843a2dadfdeaa0bce90b3698fb08f9ec6e98ef
Roland Andrag <roland.andrag@gmail.com>
2022-12-02 06:44:40 +0200

    Indentation

--- Y2022/Day01.fs
+++ Y2022/Day01.fs
@@ -4,39 +4,39 @@ open AdventOfCode
 open Helpers
 
 module Day01 =
-  let getInput () =
-    System.IO.File.ReadLines("/Users/roland/Code/AdventOfCode/Y2022/input01.txt")
-    |> Seq.map parseInt64O
-    |> Seq.fold
-         (fun state elem ->
-            match elem with
-            | None -> []::state
-            | Some i64 ->
-               let state' =
-                 match state with
-                 | [singleton] -> [i64::singleton]
-                 | head::tail -> (i64::head)::tail
-                 | [] -> state
-               state'
-       )
-         [[]]
+   let getInput () =
+      System.IO.File.ReadLines("/Users/roland/Code/AdventOfCode/Y2022/input01.txt")
+      |> Seq.map parseInt64O
+      |> Seq.fold
+            (fun state elem ->
+               match elem with
+               | None -> []::state
+               | Some i64 ->
+                  let state' =
+                     match state with
+                     | [singleton] -> [i64::singleton]
+                     | head::tail -> (i64::head)::tail
+                     | [] -> state
+                  state'
+            )
+            [[]]
 
-  module Part1 =
-    let go () =
-      getInput ()
-      |> Seq.map Seq.sum
-      |> Seq.max
-      |> p
+   module Part1 =
+      let go () =
+         getInput ()
+         |> Seq.map Seq.sum
+         |> Seq.max
+         |> p
 
-  module Part2 =
-    let go () =
-      getInput ()
-      |> Seq.map Seq.sum
-      |> Seq.sortDescending
-      |> Seq.take 3
-      |> Seq.sum
-      |> p
+   module Part2 =
+      let go () =
+         getInput ()
+         |> Seq.map Seq.sum
+         |> Seq.sortDescending
+         |> Seq.take 3
+         |> Seq.sum
+         |> p
 
 
-  let run () =
-    Part2.go ()
+   let run () =
+      Part2.go ()
