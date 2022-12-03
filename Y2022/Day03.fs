9c69d4d2c75ec49ae30a406e40c0a3ab357166c8
Roland Andrag <roland.andrag@gmail.com>
2022-12-03 09:07:14 +0200

    Extract common code to function

--- Y2022/Day03.fs
+++ Y2022/Day03.fs
@@ -5,6 +5,9 @@ open Helpers
 
 module Day03 =
 
+   let getInput () =
+      System.IO.File.ReadLines("/Users/roland/Code/AdventOfCode/Y2022/input03.txt")
+
    type System.Char with
       member c.toInt = int c
 
@@ -13,20 +16,17 @@ module Day03 =
       then c.toInt - 'a'.toInt + 1
       else c.toInt - 'A'.toInt + 27
 
-   let getInput () =
-      System.IO.File.ReadLines("/Users/roland/Code/AdventOfCode/Y2022/input03.txt")
+   let findCommonChars (s : seq<_>) =
+      s
+      |> Seq.map Seq.toSet
+      |> Seq.reduce Set.intersect
+      |> Set.toSeq
 
    module Part1 =
 
       let go () =
          getInput ()
-         |> Seq.collect (fun string ->
-               string
-               |> Seq.splitInto 2
-               |> Seq.map Seq.toSet
-               |> Seq.reduce Set.intersect
-               |> Set.toSeq
-            )
+         |> Seq.collect (Seq.splitInto 2 >> findCommonChars)
          |> Seq.sumBy score
 
    module Part2 =
@@ -34,12 +34,7 @@ module Day03 =
       let go () =
          getInput ()
          |> Seq.chunkBySize 3
-         |> Seq.collect (fun l -> // list of three strings
-               l
-               |> Seq.map Seq.toSet
-               |> Seq.reduce Set.intersect
-               |> Set.toList
-            )
+         |> Seq.collect findCommonChars
          |> Seq.sumBy score
 
    let run () =
