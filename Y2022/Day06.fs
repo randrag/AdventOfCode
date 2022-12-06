146372a984d3972fe8789a1b6ada8d927e6dd0a4
Roland Andrag <roland.andrag@gmail.com>
2022-12-06 07:18:26 +0200

    Day 6 complete

--- Y2022/Day06.fs
+++ Y2022/Day06.fs
@@ -8,18 +8,28 @@ module Day06 =
 
       let parse (lines : seq<string>) =
          lines
+         |> Seq.collect Seq.toList
          |> Seq.toList
+
+      let calculate len ll =
+         ll
+         |> List.windowed len
+         |> List.mapi (fun i l -> i+len, l, l |> List.toSet)
+         |> List.filter (fun (_i, _l, set) ->  Set.count set = len)
+         |> List.head
 
       let go (year, day) runMode =
          getInput (year, day) runMode
          |> parse
+         |> calculate 4
 
    module Part2 =
 
       let go (year, day) runMode =
          getInput (year, day) runMode
          |> Part1.parse
+         |> Part1.calculate 14
 
    let run (year, day) =
-      Example |> Part1.go (year, day) |> printn
+      Full |> Part1.go (year, day) |> printn
       Full |> Part2.go (year, day) |> printn
