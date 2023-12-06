d48fb554696b970775c4b250bc0e42bf5d52a66b
Roland Andrag <roland.andrag@agrico.co.za>
2023-12-06 06:52:01 +0200

    Prep day 6

---
+++ Y2023/Day06.fs
@@ -0,0 +1,44 @@
+namespace AdventOfCode.Y2023
+
+open Helpers
+
+module Day06 =
+
+   // Part 1
+   module Part1 =
+
+      let parse (lines : seq<string>) =
+         lines
+         |> Seq.toList
+
+      let go (year, day) runMode =
+         getInput (year, day) runMode
+         |> parse
+
+
+   // Part 2
+   module Part2 =
+
+      let go (year, day) runMode =
+         getInput (year, day) runMode
+         |> Part1.parse
+
+   // Run
+   let run (year, day) =
+      Example |> Part1.go (year, day) |> printn
+      Full |> Part2.go (year, day) |> printn
+
+
+   // Tests
+   module Test =
+      open Xunit
+      open Swensen.Unquote
+
+      open Part1
+
+      //[<Fact>]
+      //let t1 () =
+      //   let input = [""]
+      //   let expected = [""]
+      //   let actual = parse input
+      //   test <@ expected = actual @>
--- Y2023/Program.fs
+++ Y2023/Program.fs
@@ -2,5 +2,5 @@
 
   [<EntryPoint>]
   let main argv =
-      AdventOfCode.Y2023.Day05.run (2023,05)
+      AdventOfCode.Y2023.Day06.run (2023,06)
       0
